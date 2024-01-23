ad6c36c2b1f8cf9677ea81534cb8cd0e392b8351
Gilson96 <grafael99@gmail.com>
2024-01-23 16:52:29 +0000

    added new project

--- src/components/projects.js
+++ src/components/projects.js
@@ -4,19 +4,13 @@ import ProjectsInMobile from './projectsInMobile'
 
 // Assets
 import calculator from '../assets/projectsPics/calculator.png'
-import calculatorLogo from '../assets/projectsLogo/calculator_logo.png'
 import expenseTracker from '../assets/projectsPics/expenseTracker.png'
-import expenseTrackerLogo from '../assets/projectsLogo/expenseTracker_logo.png'
 import ticTacToe from '../assets/projectsPics/tic-tac-toe.png'
-import ticTacToeLogo from '../assets/projectsLogo/tic-tac-toe_logo.png'
 import toDo from '../assets/projectsPics/todo.png'
-import toDoLogo from '../assets/projectsLogo/todo_logo.png'
 import weather from '../assets/projectsPics/weather1.png'
-import weatherLogo from '../assets/projectsLogo/weather_logo.png'
 import blog from '../assets/projectsPics/blog_article.png'
-import blogLogo from '../assets/projectsLogo/blog_article_logo.png'
 import delivroo from '../assets/projectsPics/delivroo1.png'
-import delivrooLogo from '../assets/projectsLogo/delivroo_logo.png'
+import investment from '../assets/projectsPics/investment.png'
 
 const Projects = ({ back }) => {
 
@@ -28,7 +22,6 @@ const Projects = ({ back }) => {
       live: "https://gilson96.github.io/calculator/",
       github: "https://github.com/Gilson96/calculator",
       screenshot: calculator,
-      logo: calculatorLogo,
       description: "A simple calculator"
     },
     {
@@ -37,7 +30,6 @@ const Projects = ({ back }) => {
       live: " https://gilson96.github.io/expense-tracker/",
       github: "https://github.com/Gilson96/expense-tracker",
       screenshot: expenseTracker,
-      logo: expenseTrackerLogo,
       description: "A expense tracker app"
     },
 
@@ -47,7 +39,6 @@ const Projects = ({ back }) => {
       live: "https://gilson96.github.io/tic-tac-toe/",
       github: "https://github.com/Gilson96/tic-tac-toe",
       screenshot: ticTacToe,
-      logo: ticTacToeLogo,
       description: "The tic-tac-toe game"
     },
     {
@@ -56,8 +47,15 @@ const Projects = ({ back }) => {
       live: "https://gilson96.github.io/to-do/",
       github: "https://github.com/Gilson96/to-do",
       screenshot: toDo,
-      logo: toDoLogo,
       description: "A to-do list"
+    },
+    {
+      key: 5,
+      title: 'Investment Calculator',
+      live: "https://gilson96.github.io/investmentCalculator/",
+      github: "https://github.com/Gilson96/investmentCalculator",
+      screenshot: investment,
+      description: "A investement Calculator"
     },
   ]
 
@@ -68,7 +66,6 @@ const Projects = ({ back }) => {
       live: "https://blog-article-9e7b5e9272c9.herokuapp.com/",
       github: "https://github.com/Gilson96/blog-laravel",
       screenshot: blog,
-      logo: blogLogo,
       description: "A article blog."
     },
   ]
@@ -80,7 +77,6 @@ const Projects = ({ back }) => {
       live: "https://gilson96.github.io/delivoo_clone-FrontEnd/",
       github: "https://github.com/Gilson96/delivoo_clone-FrontEnd",
       screenshot: delivroo,
-      logo: delivrooLogo,
       description: "Delivroo-clone app"
     },
     {
@@ -89,7 +85,6 @@ const Projects = ({ back }) => {
       live: "https://gilson96.github.io/weather-app/",
       github: "https://github.com/Gilson96/weather-app",
       screenshot: weather,
-      logo: weatherLogo,
       description: "A weather-app"
     },
   ]
